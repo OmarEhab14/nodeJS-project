2fa8e6fb6b5bb53d2d8e13ede5b221cc31bd2e40
OmarEhab14 <www.omarehab3710@gmail.com>
2025-05-06 12:05:20 +0300

    testing discount helper functions done (unit testing done)

--- tests/discount.test.js
+++ tests/discount.test.js
@@ -3,6 +3,8 @@ const app = require('../app')
 const productModel = require('../models/product.model')
 const request = require('supertest')
 
+const {showDiscount, calculateNewPrice} = require('../helpers/discount')
+
 require('dotenv').config()
 
 beforeEach(async () => {
@@ -35,4 +37,16 @@ describe('apply discount', () => {
             });
             expect(res.status).toBe(500);
         })
+})
+
+describe('displaying discount', () => {
+    it ('should return true to show the discount', () => {
+        expect(showDiscount(20)).toBeTruthy();
+    }),
+    it ('should return false when there is no discount', () => {
+        expect(showDiscount(0)).toBeFalsy();
+    })
+    it ('should return 80.00 when the discount is 20 and the price is 100', () => {
+        expect(calculateNewPrice(100, 20)).toBe("80.00");
+    })
 })
